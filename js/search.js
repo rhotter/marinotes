fc8aed88093293ca47318941e84f1b53b9d19e3d
Raphael Hotter <rhotter@users.noreply.github.com>
2018-03-16 12:12:00 -0400

    fixed search

---
+++ js/search.js
@@ -0,0 +1,17 @@
+function filterFunction() {
+	var text = document.getElementById("myInput").value.toUpperCase();
+	var div = document.getElementById("searchDiv");
+	var a = document.getElementsByTagName("a"); // Upper case list
+
+	for(var i=0; i < a.length; i++) {
+		if (a[i].innerHTML.toUpperCase().indexOf(text) > -1) {
+			// Show it
+			a[i].style.display = "";
+			console.log(a[i])
+		} else {
+			// Don't show it
+			a[i].style.display = "none";
+		}
+	}
+	console.log("")
+}
