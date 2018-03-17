02a04cf8a4eb9e01cd487bf5bf63e560b514b355
Raphael Hotter <rhotter@users.noreply.github.com>
2018-03-17 01:07:24 -0400

    some change

--- js/search.js
+++ js/search.js
@@ -1,10 +1,14 @@
 function filterFunction() {
+	// Get rid of share button
+	document.getElementById("shareBut").style.display = "none";
+	document.getElementById("myInput").style.boxShadow = "none";
+
 	var text = document.getElementById("myInput").value.toUpperCase();
 	var div = document.getElementById("searchDiv");
 	var a = document.getElementsByTagName("a"); // Upper case list
 	var match = false;
 	for(var i=0; i < a.length-1; i++) { /* remove last a (error) */
-		if (text !== "" && a[i].innerHTML.toUpperCase().indexOf(text) > -1) {
+		if (a[i].innerHTML.toUpperCase().indexOf(text) > -1) {
 			// Show it
 			a[i].style.display = "block";
 			match = true;
@@ -13,14 +17,9 @@ function filterFunction() {
 			a[i].style.display = "none";
 		}
 	}
-	//deals with making the button dispear
-	if(text.length != 0) {
-		document.getElementById("shareBut").style.display = "none";
-		document.getElementById("myInput").style.boxShadow = "none";
-	} else {
-		document.getElementById("shareBut").style.display = "block";
-		document.getElementById("myInput").style.boxShadow = "";
-	}
+	// deals with making the button dispear
+	
+	
 	if (!match && text != "") {
 		document.getElementById("error_text").style.display = "block";
 		console.log("no match")
@@ -37,5 +36,8 @@ function erase() {
 		a[i].style.display = "none";
 	}
 	document.getElementById("myInput").style.boxShadow = "";
-
+	
+	// Re-show the share button
+	document.getElementById("shareBut").style.display = "block";
+	document.getElementById("myInput").style.boxShadow = "";
 }
