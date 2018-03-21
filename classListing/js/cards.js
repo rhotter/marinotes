40591f905778310fd7ff785a0181feb6ad37610f
merny93 <simtart@gmail.com>
2018-03-20 22:34:38 -0400

    got cards to be generated by javascript
    means we can procdeuraly generate whatever we want whenever we want

--- classListing/js/cards.js
+++ classListing/js/cards.js
@@ -1,1 +1,32 @@
 //wonderful document that deals with creating and all the shit related to the cards
+function cardGenerate(){
+  //start by creating the card holder and attributing neccesary properites
+  var divCardFull = document.createElement("div");
+  divCardFull.setAttribute('class', "cardFull");
+  //divCardFull.addEventListener("click", myFunction()); //calls function onClick
+
+  //lets add the picture div
+  var divCardPicture = document.createElement("div");
+  divCardPicture.setAttribute('class', "cardPicture");
+
+  //now for picture itself
+  var imgCard = document.createElement("img");
+  imgCard.src = "../photos/note_screenshot.jpg"; //adds picture at location picLoc
+  imgCard.style.width = "100%";
+  imgCard.style.height = "100%";
+
+  //add image to its continer
+  divCardPicture.appendChild(imgCard);
+
+  //info container
+  var divCardInfo = document.createElement("div");
+  divCardInfo.setAttribute('class', "cardInfo");
+  divCardInfo.innerHTML = "someinfor";
+
+  //add image and info to main div
+  divCardFull.appendChild(divCardPicture);
+  divCardFull.appendChild(divCardInfo);
+
+  document.getElementById("cardHolder").appendChild(divCardFull);
+
+}
