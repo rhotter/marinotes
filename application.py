977a7620b99d67e10d4f85215db41cf0a0340c9a
Raphael Hotter <rhotter@users.noreply.github.com>
2018-03-29 00:07:19 -0400

    csv stuff preliminary

--- application.py
+++ application.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, abort
+import csv
 
 app = Flask(__name__)
 
 classes = ['Calculus I', 'Calculus II', 'Calculus III', 'Linear Algebra', 'Mechanics', 'English']
 
+
 @app.route("/")
 def index():
 	return render_template("index.html", classes=classes)
 
-@app.route("/%s" % (classes[0].replace(' ','')))
-def cal():
-	return render_template("class.html")
+@app.route("/class/<course>")
+def course(course):
+	for c in classes:
+		if c.replace(' ','') == course:
+			# CSV stuff. Will need CSV files for each course
+
+			# file = open("{}.csv".format(c),'r')
+			# reader = csv.reader(file)
+			
+			# # CSV with rows as [teacher, author, upload date]
+			# cards=[]
+			# for row in reader:
+			# 	cards.append(row)
+			# file.close()
+			return render_template("class.html", course=c)
+	abort(404)
 
 
 # @app.route("/", methods=["POST"])
