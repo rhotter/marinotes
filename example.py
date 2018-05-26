83f40d6a18e83b2e647e73da7d572ed2cf93909a
Raphael Hotter <rhotter@users.noreply.github.com>
2018-05-26 11:40:50 -0400

    removed example

--- example.py
+++
@@ -1,8 +0,0 @@
-import os
-
-course = 'Calculus 1'
-teacher = 'Joseph Rinehart'
-student = 'Raffi Hotter'
-
-notes = next(os.walk('static/pdf/Notes/%s/%s/%s/.' % (course,teacher,student)))[2]
-print(notes)
