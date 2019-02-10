44e5b8f1c56de872e0d27a9928e64867862cffc4
Raffi Hotter <rhotter@users.noreply.github.com>
2019-02-10 09:59:25 -0500

    revamped db

--- application.py
+++ application.py
@@ -1,8 +1,71 @@
 from flask import Flask, render_template, redirect, abort, request
 from movement import getClasses, getInfo, getNotes, submitNote, rejectNote, acceptNote, uploadFile, deleteFile
+from flask_pymongo import PyMongo
+from config import S3_KEY, S3_SECRET, S3_BUCKET
+import boto3
 
 application = Flask(__name__)
 
+
+"""
+MongoDB functions
+"""
+
+application.config['MONGO_DBNAME'] = 'server2'
+
+with application.app_context():
+    mongo = PyMongo(application)
+    d = mongo.db.classes
+
+def getClasses():
+    classes = []
+
+    classes_curs = d.find({},{'name':1,'_id':0}).sort('name',1)
+    for cl in classes_curs:
+        classes.append(cl['name'])
+    return classes
+
+def getInfo(course):
+    teachers, students = [],[]
+
+    info = d.find_one({'name':course},{'notes':1, '_id': 0})
+    for inf in info['notes']:
+        print(inf)
+        teachers.append(inf['teacher'])
+        students.append(inf['student'])
+    return teachers,students
+
+def getNotes(course,teacher,student):
+    # Architechture here should be redone. Not optimal
+    # for many notes in same class (which shouldn't be an issue)
+    info = d.find({'name':course},{'_id':0,'notes':1})
+
+    notes=[]
+    for i in info[0]['notes']:
+        if i['teacher']==teacher and i['student']==student:
+            for j in i['files']:
+                notes.append(j['name'][:-4])
+            break;
+    notes.sort()
+    return notes
+
+"""
+Amazon S3
+"""
+def uploadFile(file, path):
+    s3_resource = boto3.resource('s3', aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket.Object(path).put(Body=file) # can put name of file here
+
+def deleteFile(file):
+	s3_resource = boto3.resource('s3', aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+	my_bucket = s3_resource.Bucket(S3_BUCKET)
+	my_bucket.Object(file).delete()
+
+
+"""
+Different routes
+"""
 @application.route("/")
 def index():
 	classes = getClasses()
